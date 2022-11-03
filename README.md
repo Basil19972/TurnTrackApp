9b97625f3aab1d65112fb091e7246cb9ebff075b
Basil <basil.peter@hotmail.ch>
2022-11-03 13:44:22 +0100

    readme erstellt

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
-Der Code wurde in Components und Pges aufgeteilt siehe (ordnerstruktur).
-Resourcen wie Bilder sind nter Public Pics zu finden.
-Einfach gesagt bestehen die pages aus einzelnen Components 
+<h1>Herzlich Willkommen zu TurnTrack:</h1>
+
+<h2>Gettig Startet:</h2>
+<ol>
+<li>Öffne ein Terminal im Root-Ordner</li>
+<li>Gib den Befehl: "npm install" ein um alle benötigten Libraries zu installieren</li>
+<li>Verwenden dn BEfehl npm start um den Dev. Server zu Starten und das Projekt einzusehen.</li>
+</ol>
+
+<h2>Backend</h2>
+   Das von mir implementierte und nötige Backend läuft auf einer gratis Instanz von Heroku und befindet sich im Standby-Modus. Das erste Anmelden b.z.w besuchen der Websiete wird ca. 30 Sekunden dauern. Danachmgeht alles flüssig.
+
+<h2>Ordner Strucktur</h2>
+
+<h3>Pages</h3>
+Im Ordenr /pages sind die gesamten Seiten der Applikation zu sehen. Diese wurden mit den diversen "Components" erstellt.
+<h3>Componets</h3>
+
+Die Components sind im Ordner /components zu finden. Diese beinhaltet alle Bestandteile der unterschiedlichen Seiten.
+
+<h2>Verwendete Technologien</h2>
+
+<ol>
+<li><h3>Frontend: React.JS</h3></li>
+<li><h3>Component Library: Mantine</h3></li>
+<li><h3>Backend: Springboot</h3></li>
+</ol>
--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -8,88 +8,92 @@ import NavBar from "../components/NavBar";
 import Userinfos from "../components/Userinfos";
 import Userstats from "../components/Userstats";
 
-  function Account() {
+function Account() {
+  const [user, setuser] = useState({
+    id: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
 
-    const [user, setuser] = useState({id:'',firstName:'',lastName:'',email:''})
-
-    async function getUser(){
-        return await axios.get('https://turn-track-production.herokuapp.com/users/current',{headers:{Authorization:`Bearer ${localStorage.getItem("AccessToken")}`}})
+  async function getUser() {
+    return await axios.get(
+      "https://turn-track-production.herokuapp.com/users/current",
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("AccessToken")}`,
+        },
       }
-
-
-
-    useEffect(()=>{
-        getUser().then(res => {      
-          setuser(res.data)
-          console.log(user.firstName)
-    
-        })},[])
-
-        
-    return (
-        <><NavBar links={[
-          {
-            "link": "/",
-            "label": "Homepage"
-        },
-          {
-            "link": "/userStatPage",
-            "label": "Training Progess"
-        },
-        {
-            "link": "/account",
-            "label": "Account"
-        },
-        {
-            "link": "/contact",
-            "label": "Contact"
-        },
-        {
-            "link": "/exercises",
-            "label": "Exercises"
-        }
-        ]} />
-
-        <BgUnterseite mess="Account" />
-
-
-
-        <Userinfos avatar={"https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/96/000000/external-user-user-tanah-basah-glyph-tanah-basah-4.png"} 
-        firstname={user.firstName} 
-        lastname={user.lastName} 
-        weight={97} 
-        height={1.87} 
-        email={user.email} 
-        age={25}/>
-
-
-        <Container mt={100}>
-        <Userstats/>
-        </Container>
-
-        
-        
-        
-        <></><Footer links={[
-        {
-          "link": "/privacypolicy",
-          "label": "Privacy Policy"
-        },
-        {
-          "link": "/imprint",
-          "label": "Imprint"
-        }
-      ]
-      } /></>
-
-
-
-
-
     );
   }
-  
-  export default Account
-  
-  
-  
+
+  useEffect(() => {
+    getUser().then((res) => {
+      setuser(res.data);
+      console.log(user.firstName);
+    });
+  }, []);
+
+  return (
+    <>
+      <NavBar
+        links={[
+          {
+            link: "/",
+            label: "Homepage",
+          },
+          {
+            link: "/userStatPage",
+            label: "Training Progess",
+          },
+          {
+            link: "/account",
+            label: "Account",
+          },
+          {
+            link: "/contact",
+            label: "Contact",
+          },
+          {
+            link: "/exercises",
+            label: "Exercises",
+          },
+        ]}
+      />
+
+      <BgUnterseite mess="Account" />
+
+      <Userinfos
+        avatar={
+          "https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/96/000000/external-user-user-tanah-basah-glyph-tanah-basah-4.png"
+        }
+        firstname={user.firstName}
+        lastname={user.lastName}
+        weight={97}
+        height={1.87}
+        email={user.email}
+        age={25}
+      />
+
+      <Container mt={100}>
+        <Userstats />
+      </Container>
+
+      <></>
+      <Footer
+        links={[
+          {
+            link: "/privacypolicy",
+            label: "Privacy Policy",
+          },
+          {
+            link: "/imprint",
+            label: "Imprint",
+          },
+        ]}
+      />
+    </>
+  );
+}
+
+export default Account;
